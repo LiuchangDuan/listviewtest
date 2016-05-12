844dd7ba7c64e01ead643a0cc2884b6d7521017c
LiuchangDuan <870121425@qq.com>
2016-05-12 20:43:02 +0800

    ListView的点击事件

--- src/com/example/listviewtest/MainActivity.java
+++ src/com/example/listviewtest/MainActivity.java
@@ -5,7 +5,11 @@ import java.util.List;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.Toast;
 
 
 public class MainActivity extends Activity {
@@ -25,6 +29,16 @@ public class MainActivity extends Activity {
         FruitAdapter adapter = new FruitAdapter(MainActivity.this, R.layout.fruit_item, fruitList);
         ListView listView = (ListView) findViewById(R.id.list_view);
         listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new OnItemClickListener() {
+
+			@Override
+			public void onItemClick(AdapterView<?> arg0, View view, int position,
+					long id) {
+				Fruit fruit = fruitList.get(position);
+				Toast.makeText(MainActivity.this, fruit.getName(), Toast.LENGTH_SHORT).show();
+			}
+        	
+		});
     }
     
     private void initFruit() {
